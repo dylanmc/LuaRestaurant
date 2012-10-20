b045d69cf7fc4d6a255404dcc3b3ebd17850fd35
Dylan McNamee <dylan@mecodegoodsomeday.com>
2012-10-20 10:24:22 -0700

    Beginning of a menu

---
+++ menu.lua
@@ -0,0 +1,90 @@
+-- menu functions
+
+menu = {	
+	drawables = {},
+	clickables = {},
+	updatables = {}
+}
+
+function menu:initialize()
+	createButtons()
+end
+
+function menu:update(dt)
+	local i = 0
+	for i in values(menu.updatables) do 
+		i:update(dt)
+	end
+end
+
+function menu:draw()
+	local i = 0
+	for i in values(menu.drawables) do 
+		i:draw()
+	end
+end
+
+function menu:mousePressed(x,y,button)
+	if button == 'l' then
+		for i in values(menu.clickables) do 
+			i:checkClick(x,y)
+		end
+	end
+end
+
+function newGameAction()
+	-- todo: re-init everything
+	mode = 5
+end
+
+function resumeAction()
+	mode = 5
+end
+
+function quitAction ()
+	os.exit()
+end
+
+function createButtons ()
+	newButton("New Game",newGameAction)
+	newButton("Resume", resumeAction)
+	newButton("Instructions", instructionsAction)
+	newButton("Quit", quitAction)
+end
+
+buttonYPos = 200
+buttonHeight = 30
+buttonXPos = 200
+
+function newButton(label, action)
+	nb = table.copy(button)
+	nb.text = label
+	nb.y = buttonYPos
+	buttonYPos = buttonYPos + buttonHeight
+	nb.x = buttonXPos
+	nb.action = action
+	table.insert(menu.drawables, nb)
+	table.insert(menu.clickables, nb)
+end
+
+button = {
+	x = 0,
+	y = 0,
+	width = 0,
+	height = 0,
+	text = "",
+	action = nil
+}
+
+function button:draw ()
+	love.graphics.print(self.text,self.x, self.y)
+end
+
+function button:checkClick(x,y)
+	if checkClick(x,y,self.x, self.y, 200, buttonHeight) then
+		print("button " .. self.text)
+		if self.action ~= nil then
+			self.action()
+		end
+	end
+end
